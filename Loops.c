b4452ffb7a142c8db23e3ba42bd4c5981c5fe1bc
HARISH MAHENDRA PAWAR <pawarharish2801@gmail.com>
2025-10-04 23:23:38 +0530

    Loops.c
    the three different types of loops used in C programming used to Display the output Jay Ganesh

---
+++ Loops.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int iCnt = 0;
+    iCnt=1;
+    for (iCnt=1 ;iCnt<=4 ; iCnt++)
+    {
+        printf("Jay Ganesh\n");
+    }
+   
+
+    //While Loop
+    iCnt=1;
+    while( iCnt<=4)
+    {
+        printf("Jay Ganesh\n");
+        iCnt++;  
+    }
+
+    //DO WHILE
+    iCnt=1;
+    do
+    {
+        printf("Jay Ganesh\n");
+        iCnt++;
+    } while (iCnt<=4);
+    
+
+    
+    return 0;
+}
