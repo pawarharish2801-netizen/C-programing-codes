fa873d1dd783318ecd9088dee2b29aaa2c001963
HARISH MAHENDRA PAWAR <pawarharish2801@gmail.com>
2025-10-04 23:24:46 +0530

    Create DynamicDemo.c
    The program explains how dynamic Memory is allocated and how we can use it to print 10 integers which we have taken input from the user

---
+++ DynamicDemo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int size = 0;
+    int *p = NULL;
+    int icnt = 0;
+
+    printf("Enter number of elements\n");
+    scanf("%d", &size);
+
+    p = (int *)malloc(size * sizeof(int));
+
+    printf("Please enter elements\n");
+    for(icnt = 0; icnt < size; icnt++)
+    {
+        scanf("%d", &p[icnt]);
+    }
+
+    printf("Entered elements are\n");
+    for(icnt = 0; icnt < size; icnt++)
+    {
+        printf("%d ", p[icnt]);
+    }
+
+    free(p);
+
+    return 0;
+}
