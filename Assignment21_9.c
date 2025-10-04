6fe224b950bfdba5546ff1773a60f54963f24906
HARISH MAHENDRA PAWAR <pawarharish2801@gmail.com>
2025-10-04 19:35:02 +0530

    Add files via upload

---
+++ Assignment21_9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    (char *) malloc (20) ;
+    (int *) malloc(24);
+    (float *)malloc(24);
+    (double *)malloc(24);
+    
+    return 0;
+}
