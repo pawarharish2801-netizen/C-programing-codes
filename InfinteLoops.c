b0b81767587259aecda60f2b4a29bbde2b3bbd16
HARISH MAHENDRA PAWAR <pawarharish2801@gmail.com>
2025-10-05 19:45:56 +0530

    InfinteLoops.c

---
+++ InfinteLoops.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{   
+    int iCnt ;
+    iCnt=1;
+    for (iCnt=1; iCnt>0 ;iCnt++)
+    {
+        printf("Jay Ganesh");
+    }
+     
+    int i;
+    i=0;
+    i=1;
+    while(i>0)
+    {
+        printf("Jay Ganesh");
+        i++;
+    }
+
+    int j=0;
+    j=1;
+    do 
+    {
+        printf("Jay Ganesh");
+        j++;   
+    }while(j>0);
+
+
+    return 0;
+}
